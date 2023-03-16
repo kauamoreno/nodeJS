3f738164be68401c0ed2426f63151c88f315391b
Aluno <kaua.moreno2005@gmail.com>
2023-03-16 10:39:45 -0300

    ex02 interface html

--- aula-16_03_2023/exercicios/ex02/ex02.js
+++ aula-16_03_2023/exercicios/ex02/ex02.js
@@ -8,4 +8,8 @@ titulo, descrição e avaliação
 fetch('http://localhost:3000/filmes').then(response => response.json())
     .then(filmes => {
         console.log(filmes);
+
+        document.getElementById('filmes1').innerHTML = `Titulo: ${filmes[0].titulo} <br> Descricão: ${filmes[0].descricao} <br> Avaliacão: ${filmes[0].avaliacao} `
+        document.getElementById('filmes2').innerHTML = `Titulo: ${filmes[1].titulo} <br> Descricão: ${filmes[1].descricao} <br> Avaliacão: ${filmes[1].avaliacao} `
+        document.getElementById('filmes3').innerHTML = `Titulo: ${filmes[2].titulo} <br> Descricão: ${filmes[2].descricao} <br> Avaliacão: ${filmes[2].avaliacao} `
     })
