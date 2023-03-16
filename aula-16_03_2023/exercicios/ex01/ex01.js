0f5f871c53ec718c14c8baa2d2a450ee456f5b3c
Aluno <kaua.moreno2005@gmail.com>
2023-03-16 10:31:09 -0300

    ex01 interface html

--- aula-16_03_2023/exercicios/ex01/ex01.js
+++ aula-16_03_2023/exercicios/ex01/ex01.js
@@ -8,4 +8,9 @@ id, nome, email e senha
 fetch('http://localhost:3000/usuarios').then(response => response.json())
     .then(users => {
         console.log(users);
+
+        document.getElementById('user1').innerHTML = `ID: ${users[0].id} <br> Nome: ${users[0].nome} <br> Email: ${users[0].email} <br> Senha: ${users[0].senha}`
+        document.getElementById('user2').innerHTML = `ID: ${users[1].id} <br> Nome: ${users[1].nome} <br> Email: ${users[1].email} <br> Senha: ${users[1].senha}`
+        document.getElementById('user3').innerHTML = `ID: ${users[2].id} <br> Nome: ${users[2].nome} <br> Email: ${users[2].email} <br> Senha: ${users[2].senha}`
+
     })
