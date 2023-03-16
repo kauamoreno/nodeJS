c51e5333f1346bda1e6e61fad91c70eaa5acb7a6
Aluno <kaua.moreno2005@gmail.com>
2023-03-16 10:46:19 -0300

    ex03 interface html

--- aula-16_03_2023/exercicios/ex03/ex03.js
+++ aula-16_03_2023/exercicios/ex03/ex03.js
@@ -8,4 +8,8 @@ nome, modelo e ano
 fetch('http://localhost:3000/carros').then(response => response.json())
     .then(carros => {
         console.log(carros);
+
+        document.getElementById('carros1').innerHTML = `Nome: ${carros[0].nome} <br> Modelo: ${carros[0].modelo} <br> Ano: ${carros[0].ano} `
+        document.getElementById('carros2').innerHTML = `Nome: ${carros[1].nome} <br> Modelo: ${carros[1].modelo} <br> Ano: ${carros[1].ano} `
+        document.getElementById('carros3').innerHTML = `Nome: ${carros[2].nome} <br> Modelo: ${carros[2].modelo} <br> Ano: ${carros[2].ano} `
     })
